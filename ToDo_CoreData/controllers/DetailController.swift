42f40b2cf9d5d956662604eaa302703e81607758
febbyviolen <febbyviolen@gmail.com>
2023-10-30 21:57:15 +0900

    .

---
+++ ToDo_CoreData/controllers/DetailController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailController.swift
+//  ToDo_CoreData
+//
+//  Created by Ebbyy on 10/30/23.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDo_CoreData/model/CoreData.swift
@@ -0,0 +1,8 @@
+//
+//  CoreData.swift
+//  ToDo_CoreData
+//
+//  Created by Ebbyy on 10/30/23.
+//
+
+import Foundation
